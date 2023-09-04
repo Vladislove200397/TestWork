1633e68d9b4835e5ea66ddb3ac13cf01da4ad966
vladislove200397 <vayp97@gmail.com>
2023-09-04 10:55:58 +0300

    init prod

---
+++ TestWork/TestWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestWork
+//
+//  Created by Vlad Kulakovsky  on 4.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
